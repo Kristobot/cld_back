18d50a865d6642069ccd15014cdb8a8c2a041c96
Cristofer <aliagacristofer3@gmail.com>
2025-06-09 00:32:26 -0500

    new seeds

--- app/priv/repo/seeds.exs
+++ app/priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+user = [
+  %{
+    email: "test@gmail.com",
+    password: "test456123",
+    role: 0,
+    person: %{
+      first_name: "Test",
+      last_name: "User",
+      address: "123 Test Street",
+      phone_number: "123456789",
+      date_of_birth: "2000-01-01",
+      specialty: 0
+    }
+  }
+]
+
+App.Accounts.create_user_with_person(user)
